1632059d54beb4ea1a826a489c6ba65f90deabc7
chadcollins <chadrcollins91@gmail.com>
2017-10-21 19:14:46 -0400

    Corrected import

--- src/components/MonthCalendar.js
+++ src/components/MonthCalendar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-import {Today, genWeeksForMonth, MonthNames} from "../utils/index";
+import {Today, genWeeksForMonth, MonthNames} from "../utils";
 
 class MonthCalendar extends React.Component {
     static propTypes = {
